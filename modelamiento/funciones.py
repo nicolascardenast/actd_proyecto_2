2e1f457b6e98a5eccb4532456afd20bbb6d46ec4
JOSE NICOLAS CARDENAS TRUJILLO <j.cardenast@uniandes.edu.co>
2024-05-06 12:13:03 -0500

    Función para calcular la proba. default

--- modelamiento/funciones.py
+++ modelamiento/funciones.py
@@ -1,14 +1,15 @@
 import tensorflow as tf     # Para la red neuronal
 import time                 # Para medir el tiempo de ejecución
+from pandas import DataFrame
 
 # Definir la función que permitirá modificar los hiperparámetros del modelo
-def cambiar_hiper(num_neur: int, fun_act: str, opti: str, datos: tuple) -> tuple:
+def cambiar_hiper(num_neur_1: int, num_neur_2: int, fun_act_1: str , fun_act_2: str, opti: str, datos: tuple) -> tuple:
     """
     Función para modificar los hiperparámetros del modelo y medir su rendimiento.
     
     Args:
-    - num_neur: Número de neuronas para la primera capa oculta.
-    - fun_act: Función de activación para la segunda capa oculta.
+    - num_neur_i: Número de neuronas para la capa oculta i.
+    - fun_act_i: Función de activación para la capa oculta i.
     - opti: Optimizador para compilar el modelo.
     - datos: Los datos para la red neuronal
     
@@ -24,21 +25,18 @@ def cambiar_hiper(num_neur: int, fun_act: str, opti: str, datos: tuple) -> tuple
     tf.keras.backend.clear_session()
     tf.random.set_seed(42)
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.InputLayer(input_shape=[X_train.shape[1]]))
+    model.add(tf.keras.layers.InputLayer(input_shape=[X_train.shape[1]]))  # Capa input
     model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(num_neur_1, activation=fun_act_1))     # Capa oculta
+    model.add(tf.keras.layers.Dense(num_neur_2, activation=fun_act_2))     # Capa oculta
+    model.add(tf.keras.layers.Dense(1, activation="sigmoid"))              # Capa output
 
-    # Primera modificación.
-    model.add(tf.keras.layers.Dense(num_neur, activation="relu"))
-
-    # Segunda modificación.
-    model.add(tf.keras.layers.Dense(100, activation=fun_act))
-    
-    model.add(tf.keras.layers.Dense(10, activation="softmax"))
+    # Información
     hidden1 = model.layers[1]
     weights, biases = hidden1.get_weights()
 
     # Tercera modificación.
-    model.compile(loss="sparse_categorical_crossentropy",
+    model.compile(loss="binary_crossentropy",
                   optimizer=opti,
                   metrics=["accuracy"])
     
@@ -57,4 +55,23 @@ def cambiar_hiper(num_neur: int, fun_act: str, opti: str, datos: tuple) -> tuple
     # Métricas para evaluar el modelo utilizando los datos de validación.
     loss, accuracy = model.evaluate(X_valid, y_valid)
 
-    return (round(loss, 4), round(accuracy, 4), round(execution_time, 2))
+    return (round(loss, 4), round(accuracy, 4), round(execution_time, 2), model)
+
+
+# Definir función que calcula probabilidad de default de un cliente
+def calc_proba(id: int, datos:DataFrame, modelo) -> float:
+    """
+    Función que calcula la probabilidad de default de un cliente.
+    
+    Args:
+    - id: ID del cliente.
+    - datos: DataFrame con los datos.
+    - modelo: el modelo predictivo
+    
+    Returns:
+    - Float: Probabilidad de default de cliente
+    """
+    client_data = datos.loc[datos['ID'] == id]
+    x_client = client_data.drop(['default_payment_next_month'], axis=1)
+    default_probability_client = modelo.predict(x_client)
+    return default_probability_client
--- modelamiento/modelamiento.py
+++ modelamiento/modelamiento.py
@@ -3,8 +3,10 @@
 ##################################
 # Esta sección contiene todas las importaciones necesarias para el script
 from pandas import read_csv
-from funciones import cambiar_hiper
+from funciones import cambiar_hiper, calc_proba
 from sklearn.model_selection import train_test_split
+from itertools import product 
+from time import time
 
 
 ##################################
@@ -13,7 +15,7 @@ from sklearn.model_selection import train_test_split
 # En esta sección se lee la información necesaria para el modelo
 ruta_datos = 'limpieza_y_alistamiento/datos_limpios.csv'
 datos = read_csv(ruta_datos)
-col_innecesarias = ['ID', 'SEX', 'EDUCATION', 'MARRIAGE', 'AGE',]
+col_innecesarias = ['SEX', 'EDUCATION', 'MARRIAGE']
 col_necesarias = [col for col in datos.columns if col not in col_innecesarias]
 datos = datos[col_necesarias]
 
@@ -29,7 +31,76 @@ y = datos['default_payment_next_month']
 # Dividir los datos en conjunto de entrenamiento, conjunto de validación y conjunto de prueba
 x_train_full, x_test, y_train_full, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 x_train, x_valid, y_train, y_valid = train_test_split(x_train_full, y_train_full, test_size=0.2, random_state=42)
+datos_modelo = x_train, x_valid, y_train, y_valid
 
-# Crear red neuronal
-datos = x_train, x_valid, y_train, y_valid
-cambiar_hiper(25, 'linear', 'adam', datos)
+'''
+## Buscar el mejor modelo ------------------------------------------------------
+# Hiperparámetros
+num_neuronas_lista = [50, 100, 200]
+funciones_activacion_lista = ['relu', 'sigmoid', 'tanh']
+optimizadores_lista = ['adam', 'sgd', 'rmsprop', 'adagrad', 'adadelta', 'nadam']
+
+# Parámetros de comparación
+mejor_perdida = float('inf')
+mejor_precision = 0
+mejores_hiperparametros = {}
+
+# Contador de iteraciones
+total_iteraciones = 0
+
+# Iniciar el temporizador
+start_time = time()
+
+# Iterar a través de todas las combinaciones de hiperparámetros
+for num_neur_1, num_neur_2, fun_act_1, fun_act_2, opti in product(num_neuronas_lista, num_neuronas_lista, funciones_activacion_lista, funciones_activacion_lista, optimizadores_lista):
+    # Incrementar el contador de iteraciones
+    total_iteraciones += 1
+    
+    # Cambiar los hiperparámetros y entrenar el modelo
+    perdida, precision, _ = cambiar_hiper(num_neur_1, num_neur_2, fun_act_1, fun_act_2, opti, datos)
+    
+    # Actualizar el mejor modelo si la pérdida mejora
+    if perdida < mejor_perdida:
+        mejor_perdida = perdida
+        mejor_precision = precision
+        mejores_hiperparametros = {'num_neur_1': num_neur_1, 'num_neur_2': num_neur_2, 'fun_act_1': fun_act_1, 'fun_act_2': fun_act_2, 'opti': opti}
+
+# Calcular el tiempo de ejecución
+tiempo_ejecucion = time() - start_time
+
+# Imprimir resultados
+print("Mejor Pérdida:", mejor_perdida)
+print("Mejor Precisión:", mejor_precision)
+print("Mejores Hiperparámetros:", mejores_hiperparametros)
+print("Total de Iteraciones:", total_iteraciones)
+print("Tiempo de Ejecución:", tiempo_ejecucion, "segundos")
+'''
+
+
+##################################
+# Sección 4: Predicciones
+##################################
+# En esta sección se realizan las predicciones
+_, _, _, modelo = cambiar_hiper(50, 50, 'relu', 'sigmoid', 'nadam', datos_modelo)
+
+
+##################################
+# Sección 5: Función del Tablero
+##################################
+# Definir función que calcula probabilidad de default de un cliente
+def proba_dado_id(id: int) -> float:
+    """
+    Función que calcula la probabilidad de default de un cliente.
+    
+    Args:
+    - id: ID del cliente.
+    
+    Returns:
+    - Float: Probabilidad de default de cliente
+    """
+    proba = calc_proba(id, datos, modelo)[0][0]
+    print("Probabilidad de default para el cliente con ID", str(id) + ":",
+           str(round(proba * 100, 2)) + '%')
+    return proba
+
+proba_dado_id(12)
